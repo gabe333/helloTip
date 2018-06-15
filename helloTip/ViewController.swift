5228d3897ce787b8e1b7b26bd4683c0a2c1297f3
Gabriel Scrob <gabe@gabriels-macbook-air.local>
2018-06-15 02:06:46 -0400

    first personal project

--- helloTip/ViewController.swift
+++ helloTip/ViewController.swift
@@ -9,17 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var totalAmount: UILabel!
+    @IBOutlet weak var textFIeld: UITextField!
+    @IBOutlet weak var tipPercent: UILabel!
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var total: UILabel!
+    @IBOutlet weak var tip: UILabel!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        
+        tipPercent.text = "\(sender.value)"
+        
+        if let totalTxt = totalAmount.text, let percentTxt = tipPercent.text{
+            if let totalAmount = Float(totalTxt), let percent = Float(percentTxt){
+                let totalBill = totalAmount * (percent / 100)
+                tip.text = "\(totalBill)"
+                let totalTip = totalAmount + totalBill
+                total.text = "\(totalTip)"
+            }
+        }
     }
-
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        
+        if let buttonAmount = textFIeld.text {
+            totalAmount.text = "\(buttonAmount)"
+            textFieldShouldReturn(textField: textFIeld)
+        }
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        self.textFIeld.resignFirstResponder()
+        return true
+    }
 
 }
 
